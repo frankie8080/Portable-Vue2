cd55eb7eec4b3ddbd2dd43d76d5004fed1d23524
fengtianxi <625362207@qq.com>
2020-11-10 17:57:16 +0800

    vscode的git提交测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <!--
  * @Author: your name
  * @Date: 2020-11-09 17:53:30
- * @LastEditTime: 2020-11-10 16:18:03
+ * @LastEditTime: 2020-11-10 17:54:24
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \elementUI-Reconsitution\src\views\Home.vue
 -->
 <template>
-    <div>hahaha</div>
+    <div>这里是home组件</div>
 </template>
 <script>
 export default {};
